90a8b49d248bd8ca6186eb54665f3addc11f36d9
zuzannagasior <50328609+zuzannagasior@users.noreply.github.com>
2020-03-15 19:42:41 +0100

    preparing app for deployment pt2

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")))
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+const uri = process.env.MONGODB_URI || process.env.ATLAS_URI;
+mongoose.connect(uri, { 
+    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -26,9 +26,14 @@ const usersRouter = require('./routes/users');
 app.use('/wishlist', wishlistRouter);
 app.use('/users', usersRouter);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
